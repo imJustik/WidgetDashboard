ef10d37ce57293703849ae387f254d9a5a2db0f5
Iliya Kuznetsov <imkuznetsov@alfabank.ru>
2019-04-30 19:52:41 +0300

    Initial Commit

---
+++ WidgetDash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WidgetDash
+//
+//  Created by Илья Кузнецов on 30/04/2019.
+//  Copyright © 2019 Илья Кузнецов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
